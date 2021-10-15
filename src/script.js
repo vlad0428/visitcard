51b10990138cfe0baaa2b773dc64b13ad15e0677
Vladislav V <0428vlad0428@mail.ru>
2021-10-15 22:40:55 +0300

    fixed problems sass style.sass style.css --watch

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import './style.css'
+import '../style.css'
 import * as THREE from 'three'
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
@@ -151,7 +151,7 @@ scene.add(pointLight, ambientLight)
 
 
 
-
-// const spaceTexture = new THREE.TextureLoader().load('space.jpg')
-// scene.background = spaceTexture
-
+//
+// // const spaceTexture = new THREE.TextureLoader().load('space.jpg')
+// // scene.background = spaceTexture
+//
